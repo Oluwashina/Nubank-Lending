d0e612f5bfd7dde766b366ec593e3f39f951e2ec
Oluwashina <shinzbaba@gmail.com>
2022-01-12 00:59:06 +0100

    get started, success and loan received page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        playfair: ["Playfair Display"],
+      },
+      colors: {
+        1: "#0066F5",
+        2: "#022B69",
+        3: "#7A889E",
+        4: "#F4F7FA",
+        5: "rgba(0, 102, 245, 0.5)",
+        6: "#E8F0FE",
+        7: "#D1DDEE",
+        8: "rgba(0,0,0,0.5)",
+        9: "rgba(0, 102, 245, 1)",
+        10: "#B4CAE7"
+      },
+      borderRadius: {
+        nubrad: "0px 0px 0px 8px"
+      }
+    },
   },
   plugins: [],
 };
