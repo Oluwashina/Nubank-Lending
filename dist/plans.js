5591405a28e6a56ba447a8ea07f44e2cbc4290de
Oluwashina <shinzbaba@gmail.com>
2022-01-17 22:41:53 +0100

    financial recommeder features scoped and integrated for test

--- dist/plans.js
+++ dist/plans.js
@@ -5,32 +5,46 @@ $(document).ready(function () {
   let occupationSubmitted = JSON.parse(occupationCheck).occupation;
 
   // check storage to see if occupation exist and the income selected from past users
+  let loan_app = JSON.parse(localStorage.getItem("loan_applications"));
+  if (loan_app && loan_app.length > 0) {
+     let loan_app_check = loan_app.some(
+       (el) => el.occupation === occupationSubmitted
+     );
+     console.log(loan_app_check);
+     // if loan app exist, check from the loan application submitted and use the plans array and make the submitted one active
+     if (loan_app_check) {
+       let p = loan_app.find((val) => val.occupation === occupationSubmitted);
+       let p_amount = p.amount;
+       let p_interest = p.interest;
+       let p_plan = p.plans;
+       var newplan = {
+         id: p.id,
+         occupation: p.occupation,
+         plans: p.plans
+       }
+       localStorage.setItem("plans", JSON.stringify(newplan));
+       // filter plans thru the selected amount
+       let filteredPlan = p_plan.filter(
+         (e) => e.amount !== p_amount.toString()
+       );
 
-  // filter occupation selected and Load data from occupation from json
-  $.getJSON("occupation.json", function (success) {
-    console.log(success);
-    //  find by occupation
-    let plans = success.find((val) => val.occupation === occupationSubmitted);
-    console.log(plans);
-    let plan = plans.plans;
-    let occupation = plan.occupation
-    for (i = 0; i < plan.length; i++) {
-      var amount = plan[i]["amount"];
-      var interest = plan[i]["interest"];
-      var view_button = `planSelect(${amount},${interest},${occupation})`;
-      var planData;
-
-      if (i == 0) {
-         planData = `
+       let firstplanData = `
             <div onClick=${view_button} class="selected w-52 h-40 item md:w-56 md:h-44 flex items-center justify-center relative rounded-md opacity-60 mx-auto md:mx-0 plan pointer-events-none">
-                <p class="text-8 text-center text-3xl price">${amount}k</p>
+                <p class="text-8 text-center text-3xl price">${p_amount}k</p>
 
                 <div class="absolute right-0 top-0 bg-10 w-16 h-12 md:w-16 md:h-12 rounded-nubrad flex justify-center items-center">
-                    <p class="text-xs text-black text-center font-medium leading-tight interest">${interest}% Interest</p>
+                    <p class="text-xs text-black text-center font-medium leading-tight interest">${p_interest}% Interest</p>
                 </div>
           </div>`;
-      } else {
-         planData = `
+       $(".plan-section").append(firstplanData);
+
+       for (i = 0; i < filteredPlan.length; i++) {
+         var amount = filteredPlan[i]["amount"];
+         var interest = filteredPlan[i]["interest"];
+         var view_button = `planSelect(${amount},${interest})`;
+         var myplanData;
+
+         myplanData = `
             <div onClick=${view_button} class="bg-6 w-52 h-40 item md:w-56 md:h-44 flex items-center justify-center relative rounded-md border-7 border opacity-60 mx-auto md:mx-0 plan pointer-events-none">
                 <p class="text-8 text-center text-3xl price">${amount}k</p>
 
@@ -38,31 +52,150 @@ $(document).ready(function () {
                     <p class="text-xs text-black text-center font-medium leading-tight interest">${interest}% Interest</p>
                 </div>
           </div>`;
-      }
-     
+         $(".plan-section").append(myplanData);
+       }
 
-      $(".plan-section").append(planData);
+       var owl = $(".owl-carousel").owlCarousel({
+         loop: true,
+         margin: 10,
+         nav: false,
+         items: 3,
+         responsive: {
+           0: {
+             items: 1,
+           },
+           600: {
+             items: 3,
+           },
+           1000: {
+             items: 3,
+           },
+         },
+       });
+       owl.init();
     }
+     else {
+       // filter occupation selected and Load data from occupation from json
+       $.getJSON("occupation.json", function (success) {
+         //  find by occupation
+         let plans = success.find(
+           (val) => val.occupation === occupationSubmitted
+         );
+         console.log(plans);
+         let plan = plans.plans;
+         localStorage.setItem("plans", JSON.stringify(plans));
 
-    var owl = $(".owl-carousel").owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: false,
-      items: 3,
-      responsive: {
-        0: {
-          items: 1,
+         for (i = 0; i < plan.length; i++) {
+           var amount = plan[i]["amount"];
+           var interest = plan[i]["interest"];
+           var view_button = `planSelect(${amount},${interest})`;
+           var planData;
+
+           if (i == 0) {
+             planData = `
+            <div onClick=${view_button} class="selected w-52 h-40 item md:w-56 md:h-44 flex items-center justify-center relative rounded-md opacity-60 mx-auto md:mx-0 plan pointer-events-none">
+                <p class="text-8 text-center text-3xl price">${amount}k</p>
+
+                <div class="absolute right-0 top-0 bg-10 w-16 h-12 md:w-16 md:h-12 rounded-nubrad flex justify-center items-center">
+                    <p class="text-xs text-black text-center font-medium leading-tight interest">${interest}% Interest</p>
+                </div>
+          </div>`;
+           } else {
+             planData = `
+            <div onClick=${view_button} class="bg-6 w-52 h-40 item md:w-56 md:h-44 flex items-center justify-center relative rounded-md border-7 border opacity-60 mx-auto md:mx-0 plan pointer-events-none">
+                <p class="text-8 text-center text-3xl price">${amount}k</p>
+
+                <div class="absolute right-0 top-0 bg-10 w-16 h-12 md:w-16 md:h-12 rounded-nubrad flex justify-center items-center">
+                    <p class="text-xs text-black text-center font-medium leading-tight interest">${interest}% Interest</p>
+                </div>
+          </div>`;
+           }
+
+           $(".plan-section").append(planData);
+         }
+
+         var owl = $(".owl-carousel").owlCarousel({
+           loop: true,
+           margin: 10,
+           nav: false,
+           items: 3,
+           responsive: {
+             0: {
+               items: 1,
+             },
+             600: {
+               items: 3,
+             },
+             1000: {
+               items: 3,
+             },
+           },
+         });
+         owl.init();
+       });
+     }
+  }
+  else {
+    // filter occupation selected and Load data from occupation from json
+    $.getJSON("occupation.json", function (success) {
+      //  find by occupation
+      let plans = success.find((val) => val.occupation === occupationSubmitted);
+      console.log(plans);
+      let plan = plans.plans;
+      localStorage.setItem("plans", JSON.stringify(plans))
+
+      for (i = 0; i < plan.length; i++) {
+        var amount = plan[i]["amount"];
+        var interest = plan[i]["interest"];
+        var view_button = `planSelect(${amount},${interest})`;
+        var planData;
+
+        if (i == 0) {
+          planData = `
+            <div onClick=${view_button} class="selected w-52 h-40 item md:w-56 md:h-44 flex items-center justify-center relative rounded-md opacity-60 mx-auto md:mx-0 plan pointer-events-none">
+                <p class="text-8 text-center text-3xl price">${amount}k</p>
+
+                <div class="absolute right-0 top-0 bg-10 w-16 h-12 md:w-16 md:h-12 rounded-nubrad flex justify-center items-center">
+                    <p class="text-xs text-black text-center font-medium leading-tight interest">${interest}% Interest</p>
+                </div>
+          </div>`;
+        } else {
+          planData = `
+            <div onClick=${view_button} class="bg-6 w-52 h-40 item md:w-56 md:h-44 flex items-center justify-center relative rounded-md border-7 border opacity-60 mx-auto md:mx-0 plan pointer-events-none">
+                <p class="text-8 text-center text-3xl price">${amount}k</p>
+
+                <div class="absolute right-0 top-0 bg-10 w-16 h-12 md:w-16 md:h-12 rounded-nubrad flex justify-center items-center">
+                    <p class="text-xs text-black text-center font-medium leading-tight interest">${interest}% Interest</p>
+                </div>
+          </div>`;
+        }
+
+        $(".plan-section").append(planData);
+      }
+
+      var owl = $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        items: 3,
+        responsive: {
+          0: {
+            items: 1,
+          },
+          600: {
+            items: 3,
+          },
+          1000: {
+            items: 3,
+          },
         },
-        600: {
-          items: 3,
-        },
-        1000: {
-          items: 3,
-        },
-      },
+      });
+      owl.init();
     });
-    owl.init();
-  });
+  }
+
+
+
 })
 
 
@@ -82,6 +215,39 @@ $('#planNo').click(function () {
    toastr.info("Kindly scroll to find a suitable plan that meets you!");
 })
 
+function SaveDataToLocalStorage(data) {
+
+  let res = JSON.parse(localStorage.getItem("loan_applications"));
+  var a;
+  // Parse the serialized data back into an aray of objects
+  a =  res || [];
+
+  // check if a partictular field is present and update
+  if (a && a.length > 0) {
+     const found = a.some((el) => el.occupation === data.occupation);
+     console.log(found);
+     if (found) {
+       const updatedItems = a.map((el) =>
+         el.occupation === data.occupation ? data : el
+       );
+       localStorage.setItem("loan_applications", JSON.stringify(updatedItems));
+    }
+     else {
+       // Push the new data (whether it be an object or anything else) onto the array
+       a.push(data);
+       // Re-serialize the array back into a string and store it in localStorage
+       localStorage.setItem("loan_applications", JSON.stringify(a));
+     }
+  }
+  else {
+    // Push the new data (whether it be an object or anything else) onto the array
+    a.push(data);
+    // Re-serialize the array back into a string and store it in localStorage
+    localStorage.setItem("loan_applications", JSON.stringify(a));
+  }
+
+}
+
 function planSelect(amt, interest) { 
   toastr.success("Thank you for applying!");
   var occupationRequest = localStorage.getItem("UserLoanRequest");
@@ -93,24 +259,17 @@ function planSelect(amt, interest) {
   setTimeout(() => {
     window.location.replace("loan-received.html");
     
+    // get plans data from storage
+    let plan = JSON.parse(localStorage.getItem("plans"));
     // save details to storage
-    var val = []
     var data = {
+      ...plan,
       occupation,
       amount: amt,
       interest
     }
-    // check if occupation exist,
-    var res = localStorage.getItem('loan_applications')
-    if (res) {
-      const found = JSON.parse(res).some((el) => el.occupation === occupation);
-       val.push(data);
-       localStorage.setItem("loan_applications", JSON.stringify(val)); 
-    }
-    else {
-      val.push(data);
-      localStorage.setItem("loan_applications", JSON.stringify(val));      
-    }
+    SaveDataToLocalStorage(data)
+
   }, 2000)
 
 }
